31bee814e14cf1b2176c7a03f2861929b8416d06
Cherif99diop <115032799+Cherif99diop@users.noreply.github.com>
2023-07-14 19:04:04 +0000

    Update cherif.py

--- cherif.py
+++ cherif.py
@@ -11,7 +11,7 @@ achats = pd.read_csv('achats.csv')
 
 merged_data = pd.merge(clics, impressions, on="cookie_id", how="right")
 merged_data = pd.merge(merged_data, achats, on="cookie_id",how="left")
-merged_data
+merged_data = merged_data.fillna("-")
 df = pd.DataFrame(merged_data)
 
 st.title("Dashboard avec Streamlit et Plotly")
