dacee6f06e66c65f8055a0a40cb79b481ea89572
Cherif99diop <115032799+Cherif99diop@users.noreply.github.com>
2023-07-14 20:15:49 +0000

    Update cherif.py

--- cherif.py
+++ cherif.py
@@ -4,7 +4,9 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.graph_objects as go
+
 # Charger les données depuis les fichiers
+
 impressions = pd.read_csv('impressions.csv')
 clics = pd.read_csv('clics.csv')
 achats = pd.read_csv('achats.csv')
@@ -15,33 +17,32 @@ df = pd.DataFrame(fusion)
 
 st.title("Dashboard avec Streamlit et Plotly")
 st.subheader("Données fusionnées")
+
 # Créer deux colonnes pour afficher les graphiques
-col1, col2 = st.columns(2)
 # Box plot de l'âge moyen en fonction des product_id
-with col1:
-    st.subheader("Box plot de l'âge moyen en fonction des product_id")
-    fig = plt.figure(figsize=(10, 6))
-    sns.boxplot(x="product_id", y="age", data=fusion)
-    plt.xlabel("product_id")
-    plt.ylabel("Âge moyen")
-    plt.title("Distribution de l'âge moyen en fonction des product_id")
-    st.pyplot(fig)
+
+st.subheader("Box plot de l'âge moyen en fonction des product_id")
+fig = plt.figure(figsize=(10, 6))
+sns.boxplot(x="product_id", y="age", data=fusion)
+plt.xlabel("product_id")
+plt.ylabel("Âge moyen")
+plt.title("Distribution de l'âge moyen en fonction des product_id")
+st.pyplot(fig)
 
 # Histogramme avec Plotly
-with col2:
-    st.title("Histogramme avec Plotly")
-    fig1 = px.histogram(fusion, x="campaign_id", y="price")
-    st.plotly_chart(fig1,width=400)
-fcol1, fcol2 = st.columns(2)  
-with fcol1:
-    st.title("Histogramme avec Plotly")
-    fig = px.histogram(fusion, x="gender", y="price")
-    st.plotly_chart(fig, width=400)
 
-with fcol2:
-    # Calcul du chiffre d'affaires
-    chiffre_affaires = df['price'].sum()
-    st.write(f"<span style='color:red; font-size:40px;'>Chiffre d'affaires : {chiffre_affaires} € </span>", unsafe_allow_html=True)
+st.title("Histogramme avec Plotly")
+fig1 = px.histogram(fusion, x="campaign_id", y="price")
+st.plotly_chart(fig1,width=400)
+
+st.title("Histogramme avec Plotly")
+fig = px.histogram(fusion, x="gender", y="price")
+st.plotly_chart(fig, width=400)
+
+# Calcul du chiffre d'affaires
+
+chiffre_affaires = df['price'].sum()
+st.write(f"<span style='color:red; font-size:40px;'>Chiffre d'affaires : {chiffre_affaires} € </span>", unsafe_allow_html=True)
 
 st.title("Histogramme avec Plotly")
 fig = px.bar(fusion, x="product_id" , y="age")
